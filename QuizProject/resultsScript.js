e65599346ef57ace91c3cb96670e5dd155506441
Landon <norogoth@gmail.com>
2020-12-23 14:46:00 -0800

    More progress. Need to fix display to be horiz.

--- QuizProject/resultsScript.js
+++ QuizProject/resultsScript.js
@@ -1,11 +1,24 @@
 const backButton = document.getElementById("back-button");
 
 function loadQuestions() {
+    //create question
     var question = document.createElement("li");
-    var node = document.createTextNode("Hi.");
-    question.appendChild(node);
-    var element = document.getElementById("qr-pairs");
-    element.appendChild(question);
+    var questionText = document.createTextNode("question");
+    question.classList.add("question");
+    question.appendChild(questionText);
+    //create answer
+    var result = document.createElement("li");
+    var resultText = document.createTextNode("WRONG!!!!");
+    result.classList.add("result");
+    result.appendChild(resultText);
+
+    //get the qr pairs element, add a new ul and add the lis above to it.
+    var qr_pairs = document.getElementById("qr-pairs");
+    var newUl = document.createElement("ul");
+    newUl.classList.add("qr_pair");
+    qr_pairs.appendChild(newUl);
+    newUl.appendChild(question);
+    newUl.appendChild(result);
 }
 
 loadQuestions();
