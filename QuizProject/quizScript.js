18e55e59b2efe289ee689abb69ca860841aa8b01
Landon <norogoth@gmail.com>
2021-01-01 10:48:39 -0800

    Pushing fully functional quiz to Branch.

--- QuizProject/quizScript.js
+++ QuizProject/quizScript.js
@@ -1,22 +1,28 @@
 const quizData = [
     {
-        question: "What is dog?",
-        a: "A babboon",
-        b: "not a dog.",
-        c: "a dog.",
-        d: "a banana?!?!",
-        correct: "c"
+        question: "What is 41 + 23?",
+        a: "58",
+        b: "81",
+        c: "64",
+        d: "67",
+        correct: "c", 
+        userChoice: "", 
+        isUserCorrect: ""
     }
     ,
     {
-        question: "What is a banana?",
-        a: "A babboon",
-        b: "not a dog.",
-        c: "a VEGETABLE?!",
-        d: "a banana?!?!",
-        correct: "d"
+        question: "What is 13 * 4?",
+        a: "61",
+        b: "42",
+        c: "48",
+        d: "52",
+        correct: "d", 
+        userChoice: "", 
+        isUserCorrect: ""
     }
 ];
+
+myStorage = window.localStorage;
 
 const question = document.getElementById('question');
 const a_text = document.getElementById('a_text');
@@ -26,10 +32,27 @@ const d_text = document.getElementById('d_text');
 const submitButton = document.getElementById('submitButton');
 const answerChoices = document.getElementsByName('answer');
 const backButton = document.getElementById("back-button");
+var radios = document.getElementsByName("name");
+var userChoice = "undefined";
 
 let currentQuestion = 0;
 
+function initQuizDate(){
+    for (let i = 0; i < quizData.length; i++){
+        myStorage.setItem(i, JSON.stringify(quizData[i]));
+    }
+    console.log(myStorage.length); //This is showing up as 3
+    for (var i = 0; i < myStorage.length - 1; i ++){
+        myStorage.setItem(i, JSON.stringify(quizData[i]));
+        console.log(i + ": " + myStorage.getItem(i) );
+    }
+    for (var i = 0; i > myStorage.length - 1; i ++){
+        console.log(myStorage.getItem(i));
+    }
+}
+
 loadQuiz();
+initQuizDate();
 
 function loadQuiz(){
     const currentQuizData = quizData[currentQuestion];
@@ -41,9 +64,34 @@ function loadQuiz(){
 }
 
 function setSelectedAnswer(){
-    for (var i = 0; i < answerChoices.length - 1; i++){
-        if (answerChoices[i].checked){
-            localStorage.setItem(i, answerChoices[i]);
+    const answers = document.getElementsByName("answer");
+    answers.forEach((answer) => {
+        if (answer.checked){
+            let userChoice = answer.id;
+            quizData[currentQuestion]["userChoice"] = userChoice;
+            if (userChoice == quizData[currentQuestion]["correct"]){
+                quizData[currentQuestion]["isUserCorrect"] = "correct";
+            }
+            else {
+                quizData[currentQuestion]["isUserCorrect"] = "incorrect";
+            }
+            console.log(quizData[currentQuestion]);
+            localStorage.setItem(currentQuestion, JSON.stringify( quizData[currentQuestion]));
+            console.log(JSON.parse(localStorage.getItem(currentQuestion)));
+        }
+    });
+}
+
+function processSelectedRadio() {
+    for (var i = 0; i < radios.length - 1; i ++){
+        if (radios[i].checked){
+            var userChoice = radios[i].id;
+            if (userChoice = quizData[currentQuestion][correct]){
+                quizData[currentQuestion][correct] = false;
+            }
+            else {
+                quizData[currentQuestion][correct] = true;
+            }
         }
     }
 }
@@ -51,6 +99,8 @@ function setSelectedAnswer(){
 submitButton.addEventListener("click", () => {
     setSelectedAnswer();
     if (quizData.length - 1 > currentQuestion){
+        quizData[currentQuestion]["userChoice"]
+        processSelectedRadio();
         currentQuestion++;
         loadQuiz();
     }
--- QuizProject/resultsScript.js
+++ QuizProject/resultsScript.js
@@ -1,19 +1,39 @@
 const backButton = document.getElementById("back-button");
+const retakeButton = document.getElementById("retake-button");
+myStorage = window.localStorage;
 
-function getQuestionAnswerPairs(){
+function setQuestionAnswerPairs(){
     var isThatAll = false;
     var i = 0;
-    while(isThatAll){
-        if (localStorage.getItem(i)){
-            
-        }
-        else {
-            isThatAll = true;
-        }
+    while(myStorage.getItem(i) != null){
+        let jsonString = myStorage.getItem(i);
+        console.log(jsonString);
+        let jsonData = JSON.parse(jsonString);
+
+        var question = document.createElement("li");
+        var questionText = document.createTextNode(jsonData.question);
+        question.classList.add("question");
+        question.appendChild(questionText);
+
+        var result = document.createElement("li");
+        var resultText = document.createTextNode(jsonData.isUserCorrect);
+        result.classList.add("result");
+        result.appendChild(resultText);
+
+        //get the qr pairs element, add a new ul and add the lis above to it.
+    var qr_pairs = document.getElementById("qr-pairs");
+    var newUl = document.createElement("ul");
+    newUl.classList.add("qr_pair");
+    qr_pairs.appendChild(newUl);
+    newUl.appendChild(question);
+    newUl.appendChild(result);
+
+        i++; 
     }
+
 }
 
-function loadQuestions() {
+function loadSampleQuestions() {
     //create question
     var question = document.createElement("li");
     var questionText = document.createTextNode("question");
@@ -34,8 +54,13 @@ function loadQuestions() {
     newUl.appendChild(result);
 }
 
-loadQuestions();
+//loadSampleQuestions();
+setQuestionAnswerPairs();
 
 backButton.addEventListener("click", () => {
     window.location.assign("https://norogoth.github.io/");
 });
+
+retakeButton.addEventListener("click", () => {
+    window.location.assign("./quiz.html")
+})
