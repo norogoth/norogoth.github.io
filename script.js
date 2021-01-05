20774eab493f8c842acaf1e73cdfbaeb72e34d03
Landon <norogoth@gmail.com>
2021-01-04 16:16:27 -0800

    Commiting first version of the Goose website.

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
+const homepageButton = document.getElementById("homepageButton");
 
-
-const myGitHubButton = document.getElementById('myGitHubButton');
-const nyCountDownButton = document.getElementById('nyCountDownButton');
-const simpleQuizButton = document.getElementById('simpleQuizButton');
-
-myGitHubButton.addEventListener("click", () => {
-    window.location.assign("https://github.com/norogoth");
-});
-
-nyCountDownButton.addEventListener("click", () => {
-    window.location.assign("./CountdownTimer/countdown.html");
-});
-
-simpleQuizButton.addEventListener("click", () => {
-    window.location.assign("./QuizProject/quiz.html")
+homepageButton.addEventListener("click", () => {
+    window.location.assign("index.html");
 });
