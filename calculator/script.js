a1048f42868f0972dc33f620a401746865e8b499
norogoth <norogoth@gmail.com>
2021-01-21 14:49:07 -0800

    comitting special code

--- calculator/script.js
+++ calculator/script.js
@@ -6,3 +6,29 @@ function getButtonById(buttonName){
     return document.querySelector(`#${buttonName}`)
 }
 
+//Respond to keyboard input. These will trigger the appropriate button function to be called.
+function keyPressed(e) {
+	let button = e.target;	
+	let buttonId = button.getId;
+	switch(buttonId) {
+		case "seven-button":
+			break;
+	}
+}
+
+window.addEventListener("keydown", keyPressed);
+window.addEventListener();
+
+//Create an object with buttons names as keys and query selectors as values. Later these query selectors will become the buttons themselves.
+const elementSelectors = {
+  addButton: "#add-button",
+  subtractButton: ".subtract-button", // whatever query selector you need
+}
+
+const elements = {}
+for (const [name, selector] of Object.entries(elementSelectors)) {
+  elements[name] = document.querySelector(selector)
+}
+
+// later
+elements.addButton.click()
