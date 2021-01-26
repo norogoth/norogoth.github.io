648954dc8f54058cffdcc79824a8296d9adcbe63
norogoth <norogoth@gmail.com>
2021-01-25 16:35:05 -0800

    almsot complete

--- calculator/script.js
+++ calculator/script.js
@@ -1,34 +1,142 @@
 //list of all my buttons
 const allButtons = window.document.getElementsByClassName("calcButton");
+const screen = window.document.getElementById("inputDisplay");
 
-//Return the specific button from the allButtons based on a string
-function getButtonById(buttonName){
-    return document.querySelector(`#${buttonName}`)
-}
+let currentValue = 0;
+let operand = null;
+let argument = null;
+let haveArg = false;
 
-//Respond to keyboard input. These will trigger the appropriate button function to be called.
-function keyPressed(e) {
-	let button = e.target;	
-	let buttonId = button.getId;
-	switch(buttonId) {
-		case "seven-button":
-			break;
+function numPress(number){
+	if (!haveArg){
+		currentValue = number;
+	}
+	if (haveArg){
+		argument = number;
 	}
 }
 
-window.addEventListener("keydown", keyPressed);
-window.addEventListener();
-
-//Create an object with buttons names as keys and query selectors as values. Later these query selectors will become the buttons themselves.
-const elementSelectors = {
-  addButton: "#add-button",
-  subtractButton: ".subtract-button", // whatever query selector you need
+function calculateNumber(){
+	switch(argument){
+	case "plus":
+		currentValue = currentValue + argument;
+		screen.value = currentValue;
+		break;
+	case "minus":
+		currentValue = currentValue - argument;
+		screen.value = currentValue;
+		break;
+	case "times":
+		currentValue = currentValue * argument;
+		screen.value = currentValue;
+		break;
+	case "dividedBy":
+		currentValue = currentValue / argument;
+		screen.value = currentValue;
+		break;
+	}
+	operand = null;
+	argument = null;
 }
 
-const elements = {}
-for (const [name, selector] of Object.entries(elementSelectors)) {
-  elements[name] = document.querySelector(selector)
+function pressNum(digit){
+	//Check if we have an operand. If we do, we will edit the argument.
+	if (operand == null){
+		typeNumber(digit, "value");
+	}
+	else {
+		typeNumber(digit, "argument");
+	}
+
+	
 }
 
-// later
-elements.addButton.click()
+function typeNumber(digit, valueOrArgString){
+	switch(valueOrArgString){
+		case "value":
+			currentValue *= 10;
+			currentValue += digit;
+			screen.value = currentValue;
+			break;
+		case "argument":
+			argument *= 10;
+			argument += digit;
+			screen.value = argument;
+			break;
+		default:
+			console.log("error on line 59");
+			break;
+	}
+	
+}
+
+function addDigitReturnValue(number, valueOrArg){
+
+}
+
+function cBC() {
+	currentValue = 0;
+	screen.value = currentValue;
+	argument = null;
+	operand = null;
+}
+function openParenBC() {
+
+}
+function closedParenBC() {
+
+}
+function divBC() {
+
+}
+function addBC(){
+	operand = "plus"
+}
+function minusBC() {
+	operand = "minus";
+}
+function multiplyBC() {
+	operand = "times";
+}
+function squareBC() {
+	operand = "square";
+	equalBC();
+}
+function sqrtBC() {
+	operand = "sqrt";
+	equalBC();
+}
+
+function divBC(){
+	operand = "divide";
+}
+function equalBC(){
+	if (operand == null){
+		//do nothing
+	}
+	else {
+		switch(operand){
+			case "plus":
+				currentValue = currentValue + argument;
+				break;
+			case "minus":
+				currentValue = currentValue - argument;
+				break;
+			case "times":
+				currentValue = currentValue * argument;
+				break;
+			case "square":
+				currentValue = currentValue * currentValue;
+				break;
+			case "sqrt":
+				currentValue = Math.sqrt(currentValue);
+				break;
+			case "divide":
+				currentValue = currentValue / argument;
+				break;
+		}
+		screen.value = currentValue;
+		operand = null;
+		argument = null;
+	}
+}
