8b0436b2017a23fa502fe418e7b10dfe7e373a49
Landon <norogoth@gmail.com>
2021-02-10 17:12:15 -0800

    Need to fix pubsub subscription.

--- ToDoList/toDoList.js
+++ ToDoList/toDoList.js
@@ -1,3 +1,4 @@
+import {pubsub} from './pubsub.js';
 
 const toDoListDiv = document.getElementById("ToDos");
 const inputNewToDo = document.getElementById("inputNewToDo");
@@ -5,15 +6,16 @@ const inputNewToDo = document.getElementById("inputNewToDo");
 //Handle input
 export const toDoList = {
     render: function() {
+        console.log("render was called.");
         document.addEventListener("keypress", function(event) {
             const key = event.key;
             if (key == "Enter"){
                 let inputText = inputNewToDo.value;
                 if (inputText != ""){
-                    this.createNewToDo(inputText);
+                    toDoList.createNewToDo(inputText);
                 }
                 else {
-                    console.log("Nothing was entered.");
+                console.log("Nothing was entered.");
                 }
                 inputNewToDo.value = "";
             }
@@ -26,7 +28,7 @@ export const toDoList = {
         newToDo.addEventListener("click", () =>{
             newToDo.remove();
         })
-        toDoList.appendChild(newToDo);
-        pubsub.publish('toDoAdded', inputText)
-    }
+        toDoListDiv.appendChild(newToDo);
+        pubsub.publish('toDoAdded', inputText);
+    },
 }
