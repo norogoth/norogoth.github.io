8869f09002fcbb64ac095fd2e4767458b1146853
Landon <norogoth@gmail.com>
2021-02-22 15:31:02 -0800

    Initial commit for react proj

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Thing/>
+  );
+}
+
+function Thing() {
+  return (
+    <div className="App">
+     hello
+    </div>
+  );
+}
+
+export default App;
