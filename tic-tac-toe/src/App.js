c219f5a5a7f37a13c9d3793884e2ef84b7bb4ae6
Landon <norogoth@gmail.com>
2021-02-22 16:46:16 -0800

    3x3 grid of ones complete

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,18 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
-function App() {
+function Box(props) {
+  return (<button>{props.number}</button>); 
+}
+function PlayGrid(props){
   return (
-    <Thing/>
-  );
+    <div>
+      <div>
+        <Box number="1"/>
+        <Box number="1"/>
+        <Box number="1"/>
+      </div>
+      <div>
+        <Box number="1"/>
+        <Box number="1"/>
+        <Box number="1"/>
+      </div>
+      <div>
+        <Box number="1"/>
+        <Box number="1"/>
+        <Box number="1"/>
+      </div>
+    </div>
+    );
 }
 
-function Thing() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+function Game() {
+  return (<PlayGrid/>);
+}
+
+function App(){
+  return(<Game/>);
 }
 
 export default App;
