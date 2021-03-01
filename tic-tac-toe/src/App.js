4df43d971841a0f16ef6d232d364ff0987ec247b
Landon <norogoth@gmail.com>
2021-03-01 13:17:14 -0800

    didsomonewin funct working?

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react'
 
+
 function Box(props) {
   return (<button className="cell" onClick={() => props.handleClick(props.index)}>{props.children}</button>); 
 }
@@ -10,15 +11,6 @@ function PlayGrid(props){
 
   return (
     <div className="grid" id="playGrid">
-        {/* <Box y/>
-        <Box handleClick={props.squareClick} className="cell" number="2"/>
-        <Box handleClick={props.squareClick} className="cell" number="3"/>
-        <Box handleClick={props.squareClick} className="cell" number="4"/>
-        <Box handleClick={props.squareClick} className="cell" number="5"/>
-        <Box handleClick={props.squareClick} className="cell" number="6"/>
-        <Box handleClick={props.squareClick} className="cell" number="7"/>
-        <Box handleClick={props.squareClick} className="cell" number="8"/>
-        <Box handleClick={props.squareClick} className="cell" number="9"/> */}
         {props.history.squares.map( (cell, index) => {
           console.log("cell: ", cell);
           return (<Box index={index} handleClick={props.squareClick} key={index} className="cell">{cell}</Box>);
@@ -28,52 +20,65 @@ function PlayGrid(props){
 }
 
 function Game() {
-  //debugger;
-  // const [history, setHistory] = useState({0: Array(9).fill(null)});
-  // const [stepNumber, setStepNumber] = useState(1);
-  // const [xIsNext, setXIsNext] = useState(true);
-
-  // function squareClick(i) {
-  //   console.log("History: " + JSON.stringify(history));
-  //   let historyCopy = {}
-  //   for (let key in history){
-  //     historyCopy[key] = history[key];
-  //   }
-  //   let latestHistory = historyCopy[stepNumber - 1].slice();
-  //   latestHistory[i] = xIsNext ? "X" : "O";
-  //   console.log("latestHistory: ",latestHistory);
-  //   historyCopy[stepNumber] = latestHistory; //This is where the new history should be inserted
-  
-  //   setHistory(historyCopy);
-  //   setStepNumber(stepNumber + 1);
-  //   setXIsNext(!xIsNext);
-
-  //     console.log("New History: " + JSON.stringify(history));
-  // }
-
   const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [turnCount, setTurnCount] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const moves = history.map((object, index) => {
+    const description = index === 0 ? "Go to start" :  "Go to move " + (index - 1);
+    return(<li>
+      <button>{description}</button>
+      </li>);
+  }); //we will use a map feature that maps each move to a button.
   
-  const turnCount = history.length % 2 == 0 ? "O" : "X";
+  function didSomeoneWin(){
+    const squares = history[history.length - 1].squares;
+    console.log("history: ", history, " squares: ", squares);
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2],
+    ];
+    lines.forEach((line) => {
+      const [a,b,c] = line;
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+        return squares[a]; // return the winner: X or O
+      }
+    })
+    return null;
+  }
 
   function squareClick(i){
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    if (squares[i]){
+      return;
+    }
     squares[i] = turnCount;
     console.log(i);
     setHistory(history.concat([{ squares: squares }]));
+    if (!didSomeoneWin()){
+      setTurnCount(history.length % 2 === 0 ? "O" : "X");   
+    }
   }
 
 
-    //OLD SQUARES CLICKED function
-    // let latestStep = history[history.length-1];//copy latest history from state
-    // latestStep["squares"][i] = history.length % 2 == 0 ? "O":"X"//edit latest history with latest move
-    // const newHistory = history.concat(latestStep); //append the latest history to my copy of history.
-    // console.log(newHistory);
-    // setHistory(newHistory); //Set history to that ^
-
-
-
-  return (<PlayGrid squareClick={squareClick} history={history[history.length - 1]}/>);
+  return (
+  <div className="row">
+    <div className="column">
+      <div>Current Turn: {turnCount}</div>
+      <ol>{moves}</ol>
+    </div>
+    <div className="column">
+      <PlayGrid squareClick={squareClick} history={history[history.length - 1]}/>
+    </div>
+  </div>
+  );
 }
 
 function App(){
