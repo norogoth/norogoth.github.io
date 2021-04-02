107e5524728e2316472e35350bdb2c180283bab3
Landon Aaker <norogoth@gmail.com>
2021-04-02 16:31:38 -0700

    commiting

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const bingoValueUrl = 'http://127.0.0.1:8000/bingo_values';
+
 function bingoCell() {
   return (<button class="bingoButton">error</button>);
 }
@@ -9,37 +11,72 @@ function bingoCell() {
 function BingoGrid(props) {
   return (
     <table id="bingoGrid">
-      <tr>
-        <td id="A1">{props.bingoData["A1"]}</td>
-        <td id="A2">{props.bingoData["A2"]}</td>
-        <td id="A3">{props.bingoData["A3"]}</td>
-      </tr>
-      <tr>
-        <td id="B1">{props.bingoData["B1"]}</td>
-        <td id="B2">{props.bingoData["B2"]}</td>
-        <td id="B3">{props.bingoData["B3"]}</td>
-      </tr>
-      <tr>
-        <td id="C1">{props.bingoData["C1"]}</td>
-        <td id="C2">{props.bingoData["C2"]}</td>
-        <td id="C3">{props.bingoData["C3"]}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td id="0">{props.values["0"]}</td>
+          <td id="1">{props.values["1"]}</td>
+          <td id="2">{props.values["2"]}</td>
+        </tr>
+        <tr>
+          <td id="3">{props.values["3"]}</td>
+          <td id="4">{props.values["4"]}</td>
+          <td id="5">{props.values["5"]}</td>
+        </tr>
+        <tr>
+          <td id="6">{props.values["6"]}</td>
+          <td id="7">{props.values["7"]}</td>
+          <td id="8">{props.values["8"]}</td>
+        </tr>
+      </tbody>
     </table>
   )
 }
 
 function App() {
   const [bingoData, setBingoData] = useState({
-    A1: "error",
-    A2: "error",
-    A3: "error",
-    B1: "error",
-    B2: "error",
-    B3: "error",
-    C1: "error",
-    C2: "error",
-    C3: "error",
   });
+  const [values, setValues] = useState({
+    0: "error",
+    1: "error",
+    2: "error",
+    3: "error",
+    4: "error",
+    5: "error",
+    6: "error",
+    7: "error",
+    8: "error",
+  })
+
+
+  function setRandomValues() {
+    console.log('bingodata after srv call: ',bingoData);
+    let newValues = values;
+    let i;
+    let numbersUsed = [];
+    for (i=0; i < 9; i++){
+      let isRepeat = true;
+      while (isRepeat){
+        const nextValue = Math.floor(Math.random() * bingoData.length);
+        if (!numbersUsed.includes(nextValue)){
+          isRepeat = false;
+          newValues[i] = nextValue; 
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetch(bingoValueUrl)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.data);
+      setBingoData(data.data);
+    })
+  }, [])
+
+  console.log('bingoData before component return',bingoData);
+  console.log('bingoValues before component return',values);
+  setRandomValues();
 
   return (
     <div className="App">
@@ -47,7 +84,7 @@ function App() {
       <p>I like the Gus and Eddy Podcast. Eddy Burback is very cool!</p>
       <button>randomize!</button>
       <span>Bingo ID: </span><input id="bingoId"></input>
-      <BingoGrid bingoData={bingoData}/>
+      <BingoGrid values={values} bingoData={bingoData}/>
     </div>
   );
 }
