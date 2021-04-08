5300b11da30a2432d7e7b8bc9bf5f79f678dccbb
Landon Aaker <norogoth@gmail.com>
2021-04-08 16:32:04 -0700

    fixed randomizer and css

--- bingo/src/BingoCell.js
+++ bingo/src/BingoCell.js
@@ -1,5 +1,23 @@
 import React, {useState, useEffect} from 'react';
 
 export default function BingoCell(props) {
-    return (<button className="bingoButton" id={props.id}>{props.displayedBD[props.id]}</button>);
+  
+  function getHead() {
+    let headStrings = ['./eddy_head.png','./gus_head.png'];
+    const headNum = Math.floor(Math.random() * 2);
+    let chosenHead = headStrings[headNum];
+    return (
+      <img src={chosenHead} />
+    );
+  }
+
+  function handleClick(id) {
+    console.log(`${id} was clicked.`);
+    let displayedBDCopy = props.displayedBD
+    displayedBDCopy[id] = 5;
+    
+    console.log(props.displayedBD[id]);
+  }
+  
+  return (<button onClick={() => handleClick(props.id)} className="bingoButton" id={props.id}>{props.displayedBD[props.id]}</button>);
   }
