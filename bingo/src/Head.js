cee6ebbf4536fa0ff8790086d7af474cc3abf82b
Landon Aaker <norogoth@gmail.com>
2021-04-09 16:28:45 -0700

    Heads working.

--- bingo/src/Head.js
+++ bingo/src/Head.js
@@ -1,11 +1,18 @@
+import React, {useState, useMemo, useEffect} from 'react';
+
 export default function Head(props) {
-    let headArray = [];
-    for (let i=0; i< 25; i++){
-        const headChoices = ['/gus_head.png','/eddy_head.png'];
-        const randInt = Math.floor(Math.random() * 2);
-        headArray[i] = headChoices[randInt];
-    }
-    console.log('Head Array: ',headArray);
-    console.log('props.id: ',props.id);
+    const headArray = useMemo(() => {
+        let headArray = [];
+        for (let i=0; i< 25; i++){
+            const headChoices = ['/gus_head.png','/eddy_head.png'];
+            const randInt = Math.floor(Math.random() * 2);
+            headArray[i] = headChoices[randInt];
+        }
+        console.log('Head Array: ',headArray);
+        console.log('props.id: ',props.id);
+        return headArray;
+    }, [props.id])
+    
+
     return (<img src={headArray[props.id]} className="headImage"/>)
 }
